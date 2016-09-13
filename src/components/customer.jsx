b68c6e80572bcdffd95db3f89615e9d1daf08603
ronen-e <alter.ronen@gmail.com>
2016-09-13 10:37:28 +0300

    Add transactions to customer

--- src/components/customer.jsx
+++ src/components/customer.jsx
@@ -4,12 +4,33 @@ export default class Customer extends Component {
     static displayName = 'Customer';
     static propTypes = {
         customer: PropTypes.object.isRequired
+    getTransactions() {
+        var customer = this.getCustomer();
+        return customer.transactions || [];
+    }
+    getCustomer() {
+        var { customers, params } = this.props;
+        var customer = customers.find( (item) => item.id === Number(params.customerId) );
+        return customer;
     }
     render() {
-        var { customer } = this.props;
+        var customer = this.getCustomer();
+        var transactions = this.getTransactions();
         return (
             <div>
-                <h2>{ customer.name }</h2>
+                <h2>{ customer.id }: { customer.name }</h2>
+                <span onClick={() => this.onDelete(customer.id)}>delete customer</span>
+                <ul className="transactions-list">
+                    {transactions.map(transaction => (
+                        <li key={ transaction.id } className="transaction-list-item">
+                            <h3>ID: { transaction.id }</h3>
+                            <h3>DATE: { transaction.purchaseDate }</h3>
+                            <h3>Item ID: { transaction.itemId }</h3>
+                            <h3>Item Name: { transaction.name }</h3>
+                            <h3>Payment ($): { transaction.price }</h3>
+                        </li>
+                    ))}
+                </ul>
             </div>
         );
     }
