c5164602ec8a37133c339f8f45dd02316678243a
ronen-e <alter.ronen@gmail.com>
2016-09-13 10:38:42 +0300

    remove deprecated

--- src/services/fetch-customers.js
+++ src/services/fetch-customers.js
@@ -1,6 +1,4 @@
 import superagent from 'superagent';
-// import { Customer } from '../../models/customer';
-// import { List as newList } from 'immutable';
 
 function fetchCustomers() {
     return new Promise((resolve, reject) => {
@@ -10,16 +8,10 @@ function fetchCustomers() {
             if (err) {
                 reject(err);
             } else {
-                // var parsedData = parse(response.body.data);
-                resolve(response.body.data.customers);
+                resolve(response.body.data);
             }
-            console.info('error', err, 'response', response);
         });
     });
 }
-
-// function parse(customers) {
-//     return newList(customers.map( (customer) => Customer.fromJSON(customer)));
-// }
 
 export default fetchCustomers;
