a2d8217a843e2fd36776ad1bee73add6ef0dee6e
ronen-e <alter.ronen@gmail.com>
2016-09-14 22:57:53 +0300

    Add TODO

--- src/app.js
+++ src/app.js
@@ -1,6 +1,18 @@
 import { initializeStore } from './state/store';
 import { render } from './render';
 
+/**
+ * TODO
+ * Server - send same response -> check if valid route
+ * Loading spinner component
+ * 404 page component
+ * Mobile adaptation
+ * Old browsers adaptation
+ * Get add customer and delete customer confirmation from backend
+ * validate new customer data
+ * npm symlinks for root level
+ * Tests
+ */
 function bootstrap() {
     var jQuery = require('jquery');
     window.jQuery = window.$ = jQuery;
